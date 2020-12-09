5be648c89e6a29db3a8ca965d8cb32b52e45132c
ibrahim-bashir-27 <ibrahim-bashir-27@users.noreply.github.com>
2020-12-08 16:18:17 -0800

    Project Setup

---
+++ helpers/utils.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+
+const validateToken = (req, res, next) => {
+    const authorizationHeaader = req.headers.authorization;
+    let result;
+    if (authorizationHeaader) {
+      const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+      const options = {
+        expiresIn: '1d',
+        issuer: ''
+      };
+      try {
+        // verify makes sure that the token hasn't expired and has been issued by us
+        result = jwt.verify(token, process.env.JWT_SECRET, options);
+
+        // Let's pass back the decoded token to the request object
+        req.decoded = result;
+        // We call next to pass execution to the subsequent middleware
+        next();
+      } catch (err) {
+        // Throw an error just in case anything goes wrong with verification
+        throw new Error(err);
+      }
+    } else {
+        result = { 
+        status: 'error',
+        message: 'Authentication error. Token required.',
+      };
+      res.status(401).send(result);
+    }
+}
+  
+
+module.exports = {
+ validateToken
+};
---
+++ loader.js
@@ -0,0 +1,26 @@
+// Packages Imports
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const cors = require("cors")
+const helmet = require("helmet")
+const compression = require("compression")
+
+
+
+
+// MiddleWares Set Up
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(compression())
+app.use(helmet())
+app.use(cors(
+    {
+        origin: '*',
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }
+))
+
+module.exports  = app
