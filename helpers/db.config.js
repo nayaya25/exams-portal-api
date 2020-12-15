692e1d62d423084668f61f40ae932601df43dd1a
ibrahim-bashir-27 <ibrahim-bashir-27@users.noreply.github.com>
2020-12-15 10:33:32 -0800

    Heroku DB URL fix

--- helpers/db.config.js
+++ helpers/db.config.js
@@ -1,22 +1,43 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } = require('./constants');
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER
+} = require("./constants");
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
-    dialect: 'postgres',
-    logging: false
-})
+let sequelize;
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+   process.env.DATABASE_URL,
+    {
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+} else {
+  sequelize = new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    {
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
+
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err.original);
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err.original);
   });
-  
+
 module.exports = {
-    sequelize,
-    Model,
-    DataTypes
-}
+  sequelize,
+  Model,
+  DataTypes,
+};
